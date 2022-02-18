8dda262f84bfcab72d3fcfeba4f558af5882daf5
Nazar Kruk <nazarkruk@icloud.com>
2022-02-17 23:47:24 -0500

    Done

--- js/challenge.js
+++ js/challenge.js
@@ -1,17 +1,15 @@
 
+let counter = document.querySelector('#counter')
+let countText = setInterval(counterUp, 1000)
 
-// let countText = setInterval(counterUp, 1000)
-
-// function counterUp(){
-//     let counter = document.querySelector('#counter')
-//     counter.innerText = parseInt(counter.innerText)+ 1
-// }
+function counterUp(){
+    counter.innerText = parseInt(counter.innerText)+ 1
+}
 
 const minus = document.querySelector('#minus')
 minus.addEventListener('click', decreseNum)
 
 function decreseNum (){
-    let counter = document.querySelector('#counter')
     counter.innerText = parseInt(counter.innerText) - 1
 }
 
@@ -19,6 +17,52 @@ const plus = document.querySelector('#plus')
 plus.addEventListener('click', increaseNum)
 
 function increaseNum (){
-    let counter = document.querySelector('#counter')
     counter.innerText = parseInt(counter.innerText) + 1
+}
+
+const heart = document.querySelector('#heart')
+heart.addEventListener('click', likeNum)
+
+function likeNum (){
+    const likes  = document.querySelector('.likes')
+    const li = document.createElement('li')
+    li.innerText = `You liked: ${parseInt(counter.innerText)}!`
+    likes.appendChild(li)
+}
+
+const pause = document.querySelector('#pause')
+pause.addEventListener('click', togglePaused)
+
+
+function togglePaused(){
+    if (pause.innerText === 'resume'){
+        countText = setInterval(counterUp, 1000)
+        pause.innerText = "pause"
+        plus.disabled = false
+        minus.disabled = false
+        heart.disabled = false
+        return
+    } else {
+        clearInterval(countText)
+        pause.innerText = "resume"
+        plus.disabled = true
+        minus.disabled = true
+        heart.disabled = true
+        return
+    }
+}
+
+const commentForm = document.querySelector('#comment-form')
+commentForm.addEventListener('submit',addComment) 
+
+const commentInput = document.querySelector('#comment-input')
+
+
+function addComment(event){
+    event.preventDefault()
+    let commentInpText = event.target.querySelector('#comment-input').value
+    const li = document.createElement('li')
+    li.innerText = commentInpText
+    const list = document.querySelector('#list')
+    list.appendChild(li)
 }
